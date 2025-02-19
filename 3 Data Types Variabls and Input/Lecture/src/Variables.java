a372f8ffc540df6e7249af5bf915efd34f20d878
Prajwal Solanke <123503783+prajwalsolanke23@users.noreply.github.com>
2025-02-19 14:33:36 +0530

    Initial commit

---
+++ 3 Data Types Variabls and Input/Lecture/src/Variables.java
@@ -0,0 +1,42 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int myNumber = 865; // initialization
+        int yourNumber; // decalaration
+
+//        System.out.println(yourNumber); // error: u did not initialize the variable
+
+        yourNumber = 865;
+
+        System.out.println(yourNumber); // 865
+
+        yourNumber = 45;
+
+        System.out.println(yourNumber); // 45
+
+        // float
+
+        float myFloat = 5.0f; // error: it's an double
+
+        System.out.println(myFloat);
+
+        // double
+
+        double myDouble = 6.84584594;
+
+        System.out.println(myDouble);
+
+        // String
+
+        String wishes = "Good Morning";
+
+        System.out.println(wishes);
+
+        // boolean
+
+        boolean isVegetarian = false;
+
+        System.out.println(isVegetarian); // false
+    }
+}
