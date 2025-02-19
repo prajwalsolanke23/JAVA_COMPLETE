ffd80f4438cc2ac8fa260d45bac9298ab3ffdc4b
Prajwal Solanke <123503783+prajwalsolanke23@users.noreply.github.com>
2025-02-19 16:18:35 +0530

    Optimized

---
+++ Lecture Code/4 Operators if else and Number System/Lecture/src/Assignment.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Assignment {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+
+        int myInt = 9;
+        System.out.println(myInt); // 9
+
+        int newInt = myInt;
+        System.out.println(newInt); // 9
+
+    }
+
+}
