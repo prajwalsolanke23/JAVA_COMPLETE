8d1239194df0d4d039dc2afd60aa350e815ce610
Prajwal Solanke <123503783+prajwalsolanke23@users.noreply.github.com>
2025-02-20 14:39:24 +0530

    added Shorthand

---
+++ Lecture Code/4 Operators if else and Number System/Lecture/src/Shorthand.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Shorthand {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int x4 = sc.nextInt();
+
+    }
+}
