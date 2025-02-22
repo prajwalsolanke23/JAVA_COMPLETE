c8939e4b9525042d9eb91ba7187c88d3b0d01987
Prajwal Solanke <123503783+prajwalsolanke23@users.noreply.github.com>
2025-02-22 15:04:06 +0530

    Update 1 Morning.java

--- Programming Challenge/src/1 Morning.java
+++ Programming Challenge/src/1 Morning.java
@@ -2,6 +2,6 @@ import java.lang.*;
 
 class Morning {
     public static void main(String[] args) {
-        System.out.print("Good Morning");
+        System.out.print("Good Morning Prajwal");
     }
 }
