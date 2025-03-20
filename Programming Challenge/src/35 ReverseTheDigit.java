8cf6b37449b7c4c2f25bf676a7a1851ea8111637
Prajwal Solanke <123503783+prajwalsolanke23@users.noreply.github.com>
2025-03-20 19:54:01 +0530

    Create 35 ReverseTheDigit.java
    added 35

---
+++ Programming Challenge/src/35 ReverseTheDigit.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class ReverseTheDigit {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Reverse the digit program.");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        int reverse = reverse(num);
+
+        System.out.println("Reverse of your number is: " + reverse);
+
+    }
+
+    public static int reverse(int num) {
+
+        int newNum = 0;
+
+        while(num > 0) {
+            int digit = num % 10 ;
+            newNum = newNum * 10 + digit;
+            num = num/10;
+        }
+
+        return newNum;
+    }
+}
